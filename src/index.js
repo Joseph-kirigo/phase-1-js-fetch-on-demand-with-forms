24efa24bade39c2c6e5c817e9ada4902351fc16e
Joseph-kirigo <josephkirigo777@gmail.com>
2022-09-07 12:07:43 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const init = () => {
-    const inputForm = document.querySelector('form')
+let init = () => {
+    let inputForm = document.querySelector('form')
   
     inputForm.addEventListener('submit', (event) => {
       event.preventDefault();
-      const input = document.querySelector('input#searchByID');
+      let input = document.querySelector('input#searchByID');
   
       fetch(`http://localhost:3000/movies/${input.value}`)
       .then(response => response.json())
       .then(data => {
-        const title = document.querySelector('section#movieDetails h4');
-        const summary = document.querySelector('section#movieDetails p');
+        let title = document.querySelector('section#movieDetails h4');
+        let summary = document.querySelector('section#movieDetails p');
   
         title.innerText = data.title;
         summary.innerText = data.summary;
